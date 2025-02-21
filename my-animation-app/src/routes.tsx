582151db69356de33cea263d6b41af0c1b23e912
Стас <stassolovyov777@gmail.com>
2025-02-21 18:02:09 +0200

    Creation of application architecture

---
+++ my-animation-app/src/routes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AnimationPage from "./pages/AnimationPage";
+
+const AppRoutes = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:animationName" element={<AnimationPage />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRoutes;
